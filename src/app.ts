5cabeb259b6767a436953d72e230addf3dd598f9
intwocave <intwocave@gmail.com>
2025-12-08 19:02:17 +0900

    [feat] 기본 app.ts

---
+++ src/app.ts
@@ -0,0 +1,18 @@
+import inquiryRoutes from './routes/inquiry-routes.js';
+import express from 'express';
+import prisma from './common/prisma.js';
+
+const app = express();
+
+// 미들웨어 설정
+app.use(express.json());
+app.use('/api', inquiryRoutes);
+
+// 에러 핸들러
+// 구현 예정
+
+// 명시적 DB 연결
+await prisma.$connect();
+console.log('데이터베이스에 성공적으로 연결됨.');
+
+export default app;
