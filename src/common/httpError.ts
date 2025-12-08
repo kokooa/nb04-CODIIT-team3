354aa0b3ea933acc02a8c13145d278885967e92e
intwocave <intwocave@gmail.com>
2025-12-08 18:42:22 +0900

    [feat] httpError 에러핸들러 메시지 기본값 설정

--- src/common/httpError.ts
+++ src/common/httpError.ts
@@ -1,6 +1,6 @@
 export class HttpError extends Error {
   constructor(
-    message: string,
+    message: string = '서버 오류가 발생했습니다.',
     public status: number = 500,
     public data?: any,
   ) {
