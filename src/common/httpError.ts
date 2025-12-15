e20d876c6ee09fd74a62c62ad0ca47cc0159d0e6
intwocave <intwocave@gmail.com>
2025-12-15 18:18:44 +0900

    [feat] httpError 에러 클래스에 error 옵션 추가

--- src/common/httpError.ts
+++ src/common/httpError.ts
@@ -3,6 +3,7 @@ export class HttpError extends Error {
     message: string = '서버 오류가 발생했습니다.',
     public status: number = 500,
     public data?: any,
+    public error?: string,
   ) {
     super(message);
   }
