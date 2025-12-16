ceb69925a8f5501bd044044780b22b964a08581d
intwocave <intwocave@gmail.com>
2025-12-16 15:15:54 +0900

    [feat] 라우터 path prefix 제거 및 product 엔드포인트 주석 처리

--- src/routes/review-router.ts
+++ src/routes/review-router.ts
@@ -4,16 +4,18 @@ import * as reviewController from '../controllers/review-controller.js';
 const router = express.Router();
 
 // 리뷰 상세 조회 (리뷰ID)
-router.get('/review/:reviewId', reviewController.getReviewDetail);
+router.get('/:reviewId', reviewController.getReviewDetail);
 
 // 리뷰 수정 (리뷰ID)
-router.patch('/review/:reviewId', reviewController.updateReview);
+router.patch('/:reviewId', reviewController.updateReview);
 
 // 리뷰 삭제 (리뷰ID)
-router.delete('/review/:reviewId', reviewController.deleteReview);
+router.delete('/:reviewId', reviewController.deleteReview);
 
 // 상품 리뷰 목록 조회 (페이지네이션 포함/상품ID)
-router.get('/product/:productId/reviews', reviewController.getReviews);
+// router.get('/product/:productId/reviews', reviewController.getReviews);
 
 // 상품 리뷰 작성 (상품ID)
-router.post('/product/:productId/reviews', reviewController.createReview);
+// router.post('/product/:productId/reviews', reviewController.createReview);
+
+export default router;
