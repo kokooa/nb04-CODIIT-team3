db4deaeb0d31fb48de583d6b45ee2cd2783d904b
intwocave <intwocave@gmail.com>
2025-12-17 17:24:49 +0900

    Revert "인증, 유저, 포인트 관련 코드 작성"

--- src/routes/metada-router.ts
+++
@@ -1,9 +0,0 @@
-// src/routes/metadata.routes.ts
-import { Router } from 'express';
-import { getGradeMetadata } from '../controllers/metadata-controller.js';
-
-const router = Router();
-
-router.get('/grade', getGradeMetadata);
-
-export default router;
