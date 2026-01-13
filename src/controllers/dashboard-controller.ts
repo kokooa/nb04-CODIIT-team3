74127c11aa0fb7706de00bdb779a095498b9b38e
kokooa <kokooa>
2026-01-13 10:32:34 +0900

    [fix] dashboard 에러 해결

--- src/controllers/dashboard-controller.ts
+++ src/controllers/dashboard-controller.ts
@@ -2,13 +2,13 @@ import express from 'express';
 type Request = express.Request;
 type Response = express.Response;
 type NextFunction = express.NextFunction;
-import { dashboardService } from '../services/dashboard-service.js';
+import { DashboardService } from '../services/dashboard-service.js';
 
 class DashboardController {
   async getAggregatedData(req: Request, res: Response, next: NextFunction) {
     try {
       const sellerId = req.user!.id;
-      const data = await dashboardService.getAggregatedDashboardData(sellerId);
+      const data = await DashboardService.getAggregatedDashboardData(sellerId);
       res.status(200).json(data);
     } catch (error) {
       next(error);
